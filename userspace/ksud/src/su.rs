3fc758c11bcfc0d56cbf218e5802d19275279781
rsuntk <rissu.ntk@gmail.com>
2024-07-30 18:46:41 +0700

    ksud: fix multiple cfg err

--- userspace/ksud/src/su.rs
+++ userspace/ksud/src/su.rs
@@ -202,7 +202,8 @@ pub fn root_shell() -> Result<()> {
     if free_idx < matches.free.len() {
         let name = &matches.free[free_idx];
         uid = unsafe {
-            #[cfg(target_arch = "aarch64", target_arch="arm")]
+        	// allow compilation on arm64 or arm
+            #[cfg(all(target_arch = "aarch64", target_arch = "arm"))]
             let pw = libc::getpwnam(name.as_ptr()).as_ref();
             #[cfg(target_arch = "x86_64")]
             let pw = libc::getpwnam(name.as_ptr() as *const i8).as_ref();
