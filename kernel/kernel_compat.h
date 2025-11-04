a59adf7cd32cb3602b2f1c60923a3372bfa114b0
Faris <rissu.ntk@gmail.com>
2025-11-04 20:18:40 +0700

    [1] kernel: migrating to new supercall implementations
    Signed-off-by: Faris <rissu.ntk@gmail.com>

--- kernel/kernel_compat.h
+++ kernel/kernel_compat.h
@@ -3,10 +3,8 @@
 
 #include <linux/fs.h>
 #include <linux/version.h>
-#include <linux/cred.h>
 #include "ss/policydb.h"
 #include "linux/key.h"
-#include <linux/list.h>
 
 #if defined(CONFIG_ARM) || defined(CONFIG_ARM64)
 #define kcompat_barrier()                                                      \
@@ -16,14 +14,6 @@
 	} while (0)
 #else
 #define kcompat_barrier() barrier()
-#endif
-
-/*
- * Linux 6.8+ does not have LKM support, due to numerous changes on LSM.
- * Let's fails if MODULE were defined.
- */
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(6, 8, 0) && defined(MODULE)
-#error "LKM mode is not supported on Linux 6.8+, aborting build."
 #endif
 
 /*
@@ -51,14 +41,13 @@ extern long ksu_strncpy_from_user_nofault(char *dst,
 					  const void __user *unsafe_addr,
 					  long count);
 extern long ksu_strncpy_from_user_retry(char *dst,
-					const void __user *unsafe_addr,
-					long count);
+					  const void __user *unsafe_addr,
+					  long count);
 
 #if LINUX_VERSION_CODE < KERNEL_VERSION(4, 10, 0) ||                           \
 	defined(CONFIG_IS_HW_HISI) || defined(CONFIG_KSU_ALLOWLIST_WORKAROUND)
 extern struct key *init_session_keyring;
 #endif
-
 extern void ksu_android_ns_fs_check(void);
 extern struct file *ksu_filp_open_compat(const char *filename, int flags,
 					 umode_t mode);
@@ -66,6 +55,9 @@ extern ssize_t ksu_kernel_read_compat(struct file *p, void *buf, size_t count,
 				      loff_t *pos);
 extern ssize_t ksu_kernel_write_compat(struct file *p, const void *buf,
 				       size_t count, loff_t *pos);
+
+extern void ksu_seccomp_clear_cache(struct seccomp_filter *filter, int nr);
+extern void ksu_seccomp_allow_cache(struct seccomp_filter *filter, int nr);
 
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 0, 0)
 #define ksu_access_ok(addr, size) access_ok(addr, size)
