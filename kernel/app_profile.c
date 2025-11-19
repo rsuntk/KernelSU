e885cff7f8fdc066ba5d6f0b8fb6df6eb698283e
fc5b87cf <rissu.ntk@gmail.com>
2025-11-19 12:50:17 +0700

    kernel: refactor disable_seccomp
    Signed-off-by: fc5b87cf <rissu.ntk@gmail.com>

--- kernel/app_profile.c
+++ kernel/app_profile.c
@@ -229,18 +229,17 @@ void disable_seccomp(struct task_struct *tsk)
 #if (LINUX_VERSION_CODE < KERNEL_VERSION(5, 9, 0) &&                           \
      defined(KSU_OPTIONAL_SECCOMP_FILTER_RELEASE))
 	seccomp_filter_release(tsk);
-#else
+#endif
 	// never, ever call seccomp_filter_release on 6.10+ (no effect)
 #if (LINUX_VERSION_CODE >= KERNEL_VERSION(5, 9, 0) &&                          \
      LINUX_VERSION_CODE < KERNEL_VERSION(6, 10, 0))
 	seccomp_filter_release(tsk);
-#else
+#endif
 #if LINUX_VERSION_CODE < KERNEL_VERSION(5, 9, 0)
 	put_seccomp_filter(tsk);
 #endif
+	// finally, we freed the filter to avoid UAF.
 	tsk->seccomp.filter = NULL;
-#endif
-#endif
 #endif
 }
 
